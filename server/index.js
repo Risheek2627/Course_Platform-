9b244a551e54f180612954a976419f3ae2e11c0e
Risheek <risheek2627.com>
2025-06-09 22:43:20 +0530

    added connection db

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
-
+const connectDB = require("./config/db");
 app.use(express.json());
 
+connectDB();
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port 3000`);
 });
