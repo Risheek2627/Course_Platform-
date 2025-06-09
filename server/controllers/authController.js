b93d81ce5490ab3b5a978145e4db066e3491fbbf
Risheek <risheek2627.com>
2025-06-09 22:42:41 +0530

    added auth controller

---
+++ server/controllers/authController.js
@@ -0,0 +1,55 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv")
+dotenv.config()
+
+const signup = async (req, res) => {
+  try {
+    const { name, email, phoneNumber, password, role } = req.body;
+
+    if (!name || !email || !phoneNumber || !password || !role) {
+      return res.status(400).json({ message: "Fields are required" });
+    }
+
+    const existUser = await User.findOne({ email });
+    if (existUser) {
+      return res.status(401).json({ message: "User already registered" });
+    }
+
+    const user = new User({ name, email, phoneNumber, password, role });
+
+    await user.save();
+
+    return res.status(200).json({ message: "User added successfully" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(401)
+        .json({ message: "Email and password is required" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+
+    const token   = jwt.sign({userId : user._id , role : user.role},{ process.env.jwt_secret})
+  } catch (error) {}
+};
+
+
+
+module.exports = {signup , login}
